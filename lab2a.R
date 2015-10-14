31ff3fc0818087f6fb359733a4063072eaa2c624
kerth18 <estherbommier@gmail.com>
2015-10-14 13:48:11 +0200

    Started the integration part and added comments

--- lab2a.R
+++ lab2a.R
@@ -1,3 +1,5 @@
+### Optimization ###
+
 f <- function(x) {(x-3)^2 + 2*(x-3)^2 + 3*(x-15)^2 + sin(100*x)}
 
 plot(f, 0, 15)
@@ -6,5 +8,20 @@ interval2 <- c(9,12)
 interval3 <- c(10,11)
 
 optimize(f, interval1)
+## $minimum 9.283052
+
 optimize(f, interval2)
+## $minimum 9.534078 
+
 optimize(f, interval3)
+## $minimum 10.16165
+
+### Integrating a function ###
+
+f2 <- function(x) {x*sin(x)}
+plot(f2, -7e5, 7e5)
+
+integrate(f2, -7e5, 7e5, subdivisions = 1e7)
+system.time(integrate(f2, -7e5, 7e5, subdivisions = 1e7))
+## user  system elapsed 
+## 32.37    0.03   34.24 
